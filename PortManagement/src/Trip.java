a30d50334c3dbebeca654adf488b3f2b86891e89
Kim <s3970589@rmit.edu.vn>
2023-09-18 16:06:30 +0700

    Trip

--- PortManagement/src/Trip.java
+++ PortManagement/src/Trip.java
@@ -1,2 +1,78 @@
 public class Trip {
+    /*
+    import java.util.Date;
+
+public class Trip {
+    private String tripID; // Unique ID for each trip
+    private String vehicleID; // ID of the vehicle making the trip
+    private String departurePortID; // ID of the departure port
+    private String arrivalPortID; // ID of the arrival port
+    private Date departureDate; // Departure date
+    private Date arrivalDate; // Arrival date
+    private String status; // Status of the trip (Completed, In-Progress, Cancelled, etc.)
+
+    public Trip(String tripID, String vehicleID, String departurePortID, String arrivalPortID, Date departureDate) {
+        this.tripID = tripID;
+        this.vehicleID = vehicleID;
+        this.departurePortID = departurePortID;
+        this.arrivalPortID = arrivalPortID;
+        this.departureDate = departureDate;
+        this.status = "In-Progress"; // Default status
+    }
+
+    // Getters and setters for all the fields
+
+    public String getTripID() {
+        return tripID;
+    }
+
+    public String getVehicleID() {
+        return vehicleID;
+    }
+
+    public String getDeparturePortID() {
+        return departurePortID;
+    }
+
+    public String getArrivalPortID() {
+        return arrivalPortID;
+    }
+
+    public Date getDepartureDate() {
+        return departureDate;
+    }
+
+    public Date getArrivalDate() {
+        return arrivalDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setArrivalDate(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    // Additional methods for operations related to a trip
+
+    // Method to mark the trip as completed
+    public void completeTrip(Date arrivalDate) {
+        this.arrivalDate = arrivalDate;
+        this.status = "Completed";
+    }
+
+    // Method to cancel a trip
+    public void cancelTrip() {
+        this.status = "Cancelled";
+    }
+
+    // Add more methods as required
+}
+
+    */
 }
